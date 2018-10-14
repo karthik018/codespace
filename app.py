548bebe4ea43719cf79a136e6e60da17db64d98d
KarthikMamidi <karthik.kumar@coalition.house>
2018-10-14 12:50:41 +0530

    added login

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def dashboard():
     return render_template('dashboard.html')
 
+@app.route('/login')
+def login():
+    render_template('templates/login.html')
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
